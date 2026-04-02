7b45b042dc93efa43c99afb61321330932a21afd
QB JIN <welcomebo@gmail.com>
2026-04-02 11:52:40 -0700

    Revert "Merge branch 'main' of https://github.com/welcomeqb/nextdaisyUI"
    This reverts commit cb5f1bf599e358e11e7a4418e4236f4fa2fd9b08, reversing
    changes made to 768be442d804408ba1d9d7dea7954541f5ab6ce2.

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,3 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {};
 
 export default nextConfig;
-
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
